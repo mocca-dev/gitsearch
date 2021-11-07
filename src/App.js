cc0976792f6d43990602506f4b0e5dfcc7e43364
Damelanote2501 <nicolastonelli1406@gmail.com>
2021-11-07 17:43:45 -0300

    First github api query attemp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import { useQuery, gql } from '@apollo/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {
+  // useQuery,
+  gql,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
 import Input from './Components/Input/Input';
 import Select from './Components/Select/Select';
@@ -13,9 +21,13 @@ function App() {
     e.preventDefault();
   };
 
+  useEffect(() => {
+    getStaticProps().then((data) => console.log(data.props.pinnedItems));
+  }, []);
+
   return (
     <div className="App">
-      <ExchangeRates />
+      {/* <ExchangeRates /> */}
       <header>
         <h2>Github user search</h2>
       </header>
@@ -40,39 +52,68 @@ function App() {
 
 export default App;
 
-const EXCHANGE_RATES = gql`
-  query {
-    repository(owner: "octocat", name: "Hello-World") {
-      issues(last: 20, states: CLOSED) {
-        edges {
-          node {
-            title
-            url
-            labels(first: 5) {
-              edges {
-                node {
-                  name
-                }
+export async function getStaticProps() {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  // repository(owner: "octocat", name: "Hello-World") {
+  //   issues(last: 20, states: CLOSED) {
+  //     edges {
+  //       node {
+  //         title
+  //         url
+  //         labels(first: 5) {
+  //           edges {
+  //             node {
+  //               name
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  const { data } = await client.query({
+    query: gql`
+      query SearchUsers($query: String!, $first: Int!) {
+        search(query: $query, type: USER, first: $first) {
+          edges {
+            node {
+              ... on User {
+                login
               }
             }
           }
         }
       }
-    }
-  }
-`;
+    `,
+    variables: {
+      query: 'mocca-dev',
+      first: 10,
+      login: '',
+    },
+  });
 
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  const { search } = data;
+  const pinnedItems = search.edges.map((edge) => edge.node);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
+  return {
+    props: { pinnedItems },
+  };
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
