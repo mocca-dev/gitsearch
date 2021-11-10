ff58a90c74a194accb7917ceb145b0a9318cdba6
Damelanote2501 <nicolastonelli1406@gmail.com>
2021-11-09 22:42:09 -0300

    First attemp of search and result

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import { useEffect, useState } from 'react';
-import {
-  // useQuery,
-  gql,
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { gql, useLazyQuery } from '@apollo/client';
 
 import './App.css';
 import Input from './Components/Input/Input';
 import Select from './Components/Select/Select';
 import SubmitButton from './Components/SubmitButton/SubmitButton';
 
+const SEARCH = gql`
+  query SearchUsers($query: String!, $first: Int!) {
+    search(query: $query, type: USER, first: $first) {
+      edges {
+        node {
+          ... on User {
+            login
+            email
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
 function App() {
   const [selectedOption, setSelectedOption] = useState('');
   const [searchText, setSearchText] = useState('');
+  const [search, { loading, error, data }] = useLazyQuery(SEARCH);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    search({
+      variables: {
+        query: searchText,
+        first: 10,
+        login: '',
+      },
+    });
+    console.log(data, searchText);
   };
-
-  useEffect(() => {
-    getStaticProps().then((data) => console.log(data.props.pinnedItems));
-  }, []);
 
   return (
     <div className="App">
-      {/* <ExchangeRates /> */}
       <header>
         <h2>Github user search</h2>
       </header>
@@ -46,74 +59,22 @@ function App() {
         />
         <SubmitButton />
       </form>
+      {loading ? (
+        <p>Loading...</p>
+      ) : data ? (
+        <ul>
+          {data?.search.edges.map(
+            (node, i) =>
+              node.node.login && (
+                <li key={node.node.login + i}>{node.node.login}</li>
+              )
+          )}
+        </ul>
+      ) : (
+        <p>There is no data returned.</p>
+      )}
     </div>
   );
 }
 
 export default App;
-
-export async function getStaticProps() {
-  const httpLink = createHttpLink({
-    uri: 'https://api.github.com/graphql',
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
-  // repository(owner: "octocat", name: "Hello-World") {
-  //   issues(last: 20, states: CLOSED) {
-  //     edges {
-  //       node {
-  //         title
-  //         url
-  //         labels(first: 5) {
-  //           edges {
-  //             node {
-  //               name
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  const { data } = await client.query({
-    query: gql`
-      query SearchUsers($query: String!, $first: Int!) {
-        search(query: $query, type: USER, first: $first) {
-          edges {
-            node {
-              ... on User {
-                login
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: {
-      query: 'mocca-dev',
-      first: 10,
-      login: '',
-    },
-  });
-
-  const { search } = data;
-  const pinnedItems = search.edges.map((edge) => edge.node);
-
-  return {
-    props: { pinnedItems },
-  };
-}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
