fe6ddee3d1698e7881c0946476d74f3f5adc5c87
Damelanote2501 <nicolastonelli1406@gmail.com>
2021-11-11 21:38:51 -0300

    SubmitButton restyling

--- src/Components/SubmitButton/SubmitButton.jsx
+++ src/Components/SubmitButton/SubmitButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import SearchIcon from './SearchIcon/SearchIcon';
+import './SubmitButton.css';
+
 const SubmitButton = () => {
   return (
     <>
-      <button type="submit">Search</button>
+      <button className="submit-button" type="submit">
+        <SearchIcon />
+      </button>
     </>
   );
 };
